818b3c2b370f6c7f10d28e55e5d204a73b50fb00
Gaston Paulich <gastopau@quilmes.com.ar>
2023-04-05 00:16:34 -0300

    consumí laptops de la base

--- src/app/laptops/laptops.component.ts
+++ src/app/laptops/laptops.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CartModelServer } from '../models/cart.model';
+import { CartService } from '../services/cart.service';
+import { ProductModel } from 'src/app/models/product.model';
+import { ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-laptops',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaptopsComponent implements OnInit {
 
-  constructor() { }
+  laptops: ProductModel[] = [];
+
+  
+
+  constructor(
+    private _productService: ProductService,
+    private _cartService: CartService
+    
+    ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(){
+    this._productService.getAllProducts()
+    .subscribe( (res: any) => {
+      this.laptops = res;
+    })
+  }
+   // adding product to cart
+   addToCart(id: string){
+    this._cartService.addToCart(id);
   }
 
 }
